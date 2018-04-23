bee2a9eb807554786fa1d4e6b52fab3b94b70a3e
Ajid Welzel <a.welzel@menger.engineering>
2018-04-23 14:25:05 +0200

    FontAwesome update

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Windows.Markup;
 // Allgemeine Informationen über eine Assembly werden über die folgenden
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
-[assembly: AssemblyTitle("FontAwesome 5 WPF")]
+[assembly: AssemblyTitle("FontAwesome 5.0.10 WPF")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FontAwesome 5 WPF")]
+[assembly: AssemblyProduct("FontAwesome 5.0.10 WPF")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -33,8 +33,8 @@ using System.Windows.Markup;
 // Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
 // indem Sie "*" wie unten gezeigt eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.2.0")]
-[assembly: AssemblyFileVersion("1.0.2.0")]
+[assembly: AssemblyVersion("1.0.3.0")]
+[assembly: AssemblyFileVersion("1.0.3.0")]
 
 
 [assembly: XmlnsPrefix("http://schemas.fontawesome.com/wpf", "fa")]
